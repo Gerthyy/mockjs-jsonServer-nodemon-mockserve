840b4e562e360bc746805c870a44a45fc7b4846e
Gerth Wang <gerth.wang@henkel.com>
2021-06-01 12:55:37 +0800

    update data file

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 /*
  * @Author: gerthwang
  * @Date:   2021-05-31 10:17:33
- * @Last Modified by:   gerthwang
- * @Last Modified time: 2021-05-31 17:52:35
+ * @Last Modified by:   Gerth Wang
+ * @Last Modified time: 2021-06-01 12:55:06
  */
 let Mock = require("mockjs");
 let Random = Mock.Random;
@@ -17,33 +17,42 @@ module.exports = () => {
 
   for (let i = 1; i <= 100; i++) {
     let part1 = Mock.mock({
-      "part|1": ["hair",],
+      "part|1": ["hair","eyeshadow"],
     });
-    var id1 =  Random.id();
+    let textures = Mock.mock({
+      "textures|1": ["liquid","powered","cream"],
+    });
+    let fnish = Mock.mock({
+      "fnish|1": ["matter","glossy"],
+    });
+    var id1 =  Random.guid();
     var config =
 
       part1.part === "hair"
         ? Mock.mock({
-            "labColor|1": [
+            "shades|1": [
               [89, -119, -100],
               [100, 89, 121],
               [50, 21, 54],
               [23, 43, 76],
               [54, 43, 76],
             ],
+            "shape":[1,2,3,4,5,6,7],
             "shining|1-100": 100,
             "grey|1-100": 50,
             "strength|1-100": 100,
-            textures: {
-              number:Random.int(0,100),
-            }
+            ...textures,
+            ...fnish
           })
         : part1.part === "eyeshadow"
         ? Mock.mock({
-            "rgbArray|9": [Random.rgb()],
+            "shades|9": [[123,234,145]],
+            "shape":[1,2,3,4,5,6,7],
             "shining|1-100": 100,
             "grey|1-100": 50,
             "strength|1-100": 100,
+            ...textures,
+            ...fnish
           })
         : {};
     var data1 = {
